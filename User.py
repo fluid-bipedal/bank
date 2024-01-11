a2f97d0a5658d61663a5fd8e09c1e34cd33c8197
Sten Lori <152862227+fluid-bipedal@users.noreply.github.com>
2024-01-11 17:58:52 +0600

    Create User.py

---
+++ User.py
@@ -0,0 +1,11 @@
+# User.py
+class User:
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def get_username(self):
+        return self.username
+
+    def get_email(self):
+        return self.email
